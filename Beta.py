e4e942a3f518bf4b95d5c67a20a32bbf5544f3d7
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2023-09-20 16:43:20 +0600

    Create Beta.py

---
+++ Beta.py
@@ -0,0 +1,22 @@
+#Programme By Mr. Beta
+import os
+os.system('clear')
+print('- Duplicate Remover By Mr. Beta -')
+
+
+def rd():
+  try:
+      file = input('File Path >> ')
+      check = open(file)
+  except:
+    exit('File Not Found ! ')
+  save = input('New Path >> ')
+  print('Duplicate Removing....')
+  lines_seen = set()
+  with open(file, 'r') as infile, open(save, 'w') as outfile:
+      for line in infile:
+          if line not in lines_seen:
+             outfile.write(line)
+             lines_seen.add(line)
+              
+rd()
