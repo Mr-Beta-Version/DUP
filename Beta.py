eb4d78b6f338cbb4b8e4107e3f1597d47db0c642
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2023-09-27 14:20:20 +0600

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -1,11 +1,44 @@
 #Programme By Mr. Beta
-import os
+import os,time,random
 os.system('git pull')
 os.system('clear')
-print('- Duplicate Remover By Mr. Beta -')
+
+def menu():
+  os.system('clear')
+  print('[1] Remove Duplicate')
+  print('[2] Mix A File')
+  ch = input('>> ')
+  if ch=='1':
+    rd()
+  elif ch=='2':
+    mx()
+  else:
+    print('Invalid Choice')
+    time.sleep(3)
+    menu()
+
+
+def mx():
+  os.system('clear')
+  print('- File Mixer By Mr. Beta -')
+  try:
+      file = input('File Path >> ')
+      check = open(file)
+  except:
+    exit('File Not Found ! ')
+  save = input('New Path >> ')
+  print('Mixing IDs....')
+  idslist = open(file).read().splitlines()
+  random.shuffle(idslist)
+  for id in idslist:
+    open(save,'a').write(id+'\n')
+  print(f'File Mix Successful > {save}')
+              
 
 
 def rd():
+  os.system('clear')
+  print('- Duplicate Remover By Mr. Beta -')
   try:
       file = input('File Path >> ')
       check = open(file)
@@ -19,6 +52,6 @@ def rd():
           if line not in lines_seen:
              outfile.write(line)
              lines_seen.add(line)
-  print('Duplicate Remove Successful')
+  print(f'Duplicate Remove Successful > {save}')
               
 rd()
